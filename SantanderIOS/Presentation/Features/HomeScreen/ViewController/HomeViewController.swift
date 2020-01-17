7c4eb01215fbd79f52f7276e5c69425b6d14a919
Daniel Farage <daniel.farage@yahoo.com.br>
2020-01-16 23:38:57 -0300

    Tela de login criada

---
+++ SantanderIOS/Presentation/Features/HomeScreen/ViewController/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SantanderIOS
+//
+//  Created by Daniel Farage on 15/01/20.
+//  Copyright © 2020 Daniel Farage. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
