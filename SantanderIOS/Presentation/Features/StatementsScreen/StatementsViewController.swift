dc3dc813f14719fca348d28945205522d100dec0
Daniel Farage <daniel.farage@yahoo.com.br>
2020-01-20 23:00:57 -0300

    Telas e viewControllers default criadas.

---
+++ SantanderIOS/Presentation/Features/StatementsScreen/StatementsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatementsViewController.swift
+//  SantanderIOS
+//
+//  Created by Daniel Farage on 20/01/20.
+//  Copyright © 2020 Daniel Farage. All rights reserved.
+//
+
+import UIKit
+
+class StatementsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
